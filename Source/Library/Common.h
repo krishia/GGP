694f2a13d1b1512db24d54135d6656d7b472dcc0
SeongraeNoh <rhosunr99@khu.ac.kr>
2022-03-28 16:26:43 +0900

    [LAB02]

--- Source/Library/Common.h
+++ Source/Library/Common.h
@@ -17,12 +17,15 @@
 
 #include <windows.h>
 #include <wrl.h>
+
 #include <d3d11_4.h>
 #include <directxcolors.h>
+
+#include <memory>
 
 #include "Resource.h"
 
 constexpr LPCWSTR PSZ_COURSE_TITLE = L"Game Graphics Programming";
 
-using namespace Microsoft::WRL;
 using namespace DirectX;
+using namespace Microsoft::WRL;
